4151a518a11ba533a23098800121dd1f74245f58
Christian Wellenbrock <christian.wellenbrock@gmail.com>
2013-07-02 11:46:16 +0200

    Add AESessionContext

---
+++ AdjustIo/AESessionContext.h
@@ -0,0 +1,13 @@
+//
+//  AESessionContext.h
+//  AdjustIosApp
+//
+//  Created by Christian Wellenbrock on 01.07.13.
+//  Copyright (c) 2013 adeven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AESessionContext : NSMutableDictionary
+
+@end
---
+++ AdjustIo/AESessionContext.m
@@ -0,0 +1,13 @@
+//
+//  AESessionContext.m
+//  AdjustIosApp
+//
+//  Created by Christian Wellenbrock on 01.07.13.
+//  Copyright (c) 2013 adeven. All rights reserved.
+//
+
+#import "AESessionContext.h"
+
+@implementation AESessionContext
+
+@end
