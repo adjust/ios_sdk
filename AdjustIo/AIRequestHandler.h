c9d3241e79b5ae1874778172183a5c936132788d
Christian Wellenbrock <christian.wellenbrock@gmail.com>
2013-07-04 19:10:38 +0200

    Add RequestHandler

---
+++ AdjustIo/AIRequestHandler.h
@@ -0,0 +1,13 @@
+//
+//  AIRequestHandler.h
+//  AdjustIosApp
+//
+//  Created by Christian Wellenbrock on 04.07.13.
+//  Copyright (c) 2013 adeven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AIRequestHandler : NSObject
+
+@end
---
+++ AdjustIo/AIRequestHandler.m
@@ -0,0 +1,13 @@
+//
+//  AIRequestHandler.m
+//  AdjustIosApp
+//
+//  Created by Christian Wellenbrock on 04.07.13.
+//  Copyright (c) 2013 adeven. All rights reserved.
+//
+
+#import "AIRequestHandler.h"
+
+@implementation AIRequestHandler
+
+@end
