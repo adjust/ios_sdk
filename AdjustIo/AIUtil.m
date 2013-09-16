11bac25c68e969a96ea30bfc9afaaa78897a636c
Christian Wellenbrock <christian.wellenbrock@gmail.com>
2013-09-16 18:25:50 +0200

    Update version to 2.1.0

--- AdjustIo/AIUtil.m
+++ AdjustIo/AIUtil.m
@@ -10,7 +10,7 @@
 #import "UIDevice+AIAdditions.h"
 
 static NSString * const kBaseUrl   = @"https://app.adjust.io";
-static NSString * const kClientSdk = @"ios2.0.1";
+static NSString * const kClientSdk = @"ios2.1.0";
 
 
 #pragma mark -
