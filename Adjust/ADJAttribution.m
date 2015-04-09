2bff9b8343687d09edb94d4f6806d2fc9d67368f
Pedro <pedro@adeven.com>
2015-04-09 15:54:57 +0200

    Rename click label log

--- Adjust/ADJAttribution.m
+++ Adjust/ADJAttribution.m
@@ -98,7 +98,7 @@
 }
 
 - (NSString *)description {
-    return [NSString stringWithFormat:@"tt:%@ tn:%@ net:%@ cam:%@ adg:%@ cre:%@ lab:%@",
+    return [NSString stringWithFormat:@"tt:%@ tn:%@ net:%@ cam:%@ adg:%@ cre:%@ clklbl:%@",
             self.trackerToken, self.trackerName, self.network, self.campaign,
             self.adgroup, self.creative, self.clickLabel];
 }
