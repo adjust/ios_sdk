e885c811fdd560c08b17f285e29dac78fbf3dff7
Alexander Kolov <me@alexkolov.com>
2015-05-08 13:39:50 +0200

    Dynamic framework

---
+++ Adjust/AdjustFramework.h
@@ -0,0 +1,20 @@
+//
+//  Adjust.h
+//  Adjust
+//
+//  Created by Alexander Kolov on 8.5.15.
+//  Copyright (c) 2015 adjust GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Adjust.
+FOUNDATION_EXPORT double AdjustVersionNumber;
+
+//! Project version string for Adjust.
+FOUNDATION_EXPORT const unsigned char AdjustVersionString[];
+
+#import <Adjust/AdjustMain.h>
+#import <Adjust/ADJEvent.h>
+#import <Adjust/ADJAttribution.h>
+#import <Adjust/ADJConfig.h>
---
+++ AdjustFramework/AdjustFramework.h
@@ -0,0 +1,20 @@
+//
+//  AdjustFramework.h
+//  adjust
+//
+//  Created by Alexander Kolov on 8.5.15.
+//  Copyright (c) 2015 adjust GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AdjustFramework.
+FOUNDATION_EXPORT double AdjustVersionNumber;
+
+//! Project version string for AdjustFramework.
+FOUNDATION_EXPORT const unsigned char AdjustVersionString[];
+
+#import <Adjust/Adjust.h>
+#import <Adjust/ADJEvent.h>
+#import <Adjust/ADJAttribution.h>
+#import <Adjust/ADJConfig.h>
