184cb4aac3471824966ead914844b14448c266b3
uerceg <ugljesa@adjust.com>
2015-11-13 12:07:39 +0100

    Adding dynamic framework

---
+++ AdjustDynamic/AdjustDynamic.h
@@ -0,0 +1,23 @@
+//
+//  AdjustDynamic.h
+//  AdjustDynamic
+//
+//  Created by Uglješa Erceg on 13/11/15.
+//  Copyright © 2015 adjust GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AdjustDynamic.
+FOUNDATION_EXPORT double AdjustDynamicVersionNumber;
+
+//! Project version string for AdjustDynamic.
+FOUNDATION_EXPORT const unsigned char AdjustDynamicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdjustDynamic/PublicHeader.h>
+
+#import <AdjustDynamic/Adjust.h>
+#import <AdjustDynamic/ADJEvent.h>
+#import <AdjustDynamic/ADJConfig.h>
+#import <AdjustDynamic/ADJLogger.h>
+#import <AdjustDynamic/ADJAttribution.h>
