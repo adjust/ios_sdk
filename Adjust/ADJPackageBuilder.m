7f9821cf94d51a0560fc9d5881eb91450ea627b3
Pedro <pedro@adeven.com>
2016-05-27 14:24:40 +0200

    Parameter builder now static

--- Adjust/ADJPackageBuilder.m
+++ Adjust/ADJPackageBuilder.m
@@ -48,10 +48,8 @@
 
 - (ADJActivityPackage *)buildSessionPackage {
     NSMutableDictionary *parameters = [self defaultParameters];
-
-    [self parameters:parameters setDuration:self.activityState.lastInterval forKey:@"last_interval"];
-    [self parameters:parameters setString:self.adjustConfig.defaultTracker forKey:@"default_tracker"];
-
+    [ADJPackageBuilder parameters:parameters setDuration:self.activityState.lastInterval forKey:@"last_interval"];
+    [ADJPackageBuilder parameters:parameters setString:self.adjustConfig.defaultTracker forKey:@"default_tracker"];
     ADJActivityPackage *sessionPackage = [self defaultActivityPackage];
     sessionPackage.path = @"/session";
     sessionPackage.activityKind = ADJActivityKindSession;
@@ -63,25 +61,23 @@
 
 - (ADJActivityPackage *)buildEventPackage:(ADJEvent *)event {
     NSMutableDictionary *parameters = [self defaultParameters];
+    [ADJPackageBuilder parameters:parameters setInt:self.activityState.eventCount forKey:@"event_count"];
+    [ADJPackageBuilder parameters:parameters setNumber:event.revenue forKey:@"revenue"];
+    [ADJPackageBuilder parameters:parameters setString:event.currency forKey:@"currency"];
+    [ADJPackageBuilder parameters:parameters setString:event.eventToken forKey:@"event_token"];
 
-    [self parameters:parameters setInt:self.activityState.eventCount forKey:@"event_count"];
-    [self parameters:parameters setNumber:event.revenue forKey:@"revenue"];
-    [self parameters:parameters setString:event.currency forKey:@"currency"];
-    [self parameters:parameters setString:event.eventToken forKey:@"event_token"];
-
-    [self parameters:parameters setDictionary:event.callbackParameters forKey:@"callback_params"];
-    [self parameters:parameters setDictionary:event.partnerParameters forKey:@"partner_params"];
+    [ADJPackageBuilder parameters:parameters setDictionary:event.callbackParameters forKey:@"callback_params"];
+    [ADJPackageBuilder parameters:parameters setDictionary:event.partnerParameters forKey:@"partner_params"];
 
     if (event.emptyReceipt) {
         NSString *emptyReceipt = @"empty";
-
-        [self parameters:parameters setString:emptyReceipt forKey:@"receipt"];
-        [self parameters:parameters setString:event.transactionId forKey:@"transaction_id"];
-    } else if (event.receipt != nil) {
+        [ADJPackageBuilder parameters:parameters setString:emptyReceipt forKey:@"receipt"];
+        [ADJPackageBuilder parameters:parameters setString:event.transactionId forKey:@"transaction_id"];
+    }
+    else if (event.receipt != nil) {
         NSString *receiptBase64 = [event.receipt adjEncodeBase64];
-
-        [self parameters:parameters setString:receiptBase64 forKey:@"receipt"];
-        [self parameters:parameters setString:event.transactionId forKey:@"transaction_id"];
+        [ADJPackageBuilder parameters:parameters setString:receiptBase64 forKey:@"receipt"];
+        [ADJPackageBuilder parameters:parameters setString:event.transactionId forKey:@"transaction_id"];
     }
 
     ADJActivityPackage *eventPackage = [self defaultActivityPackage];
@@ -96,21 +92,20 @@
 - (ADJActivityPackage *)buildClickPackage:(NSString *)clickSource {
     NSMutableDictionary *parameters = [self idsParameters];
 
-    [self parameters:parameters setString:clickSource                   forKey:@"source"];
-    [self parameters:parameters setDictionary:self.deeplinkParameters   forKey:@"params"];
-    [self parameters:parameters setDate:self.clickTime                  forKey:@"click_time"];
-    [self parameters:parameters setDate:self.purchaseTime               forKey:@"purchase_time"];
+    [ADJPackageBuilder parameters:parameters setString:clickSource                     forKey:@"source"];
+    [ADJPackageBuilder parameters:parameters setDictionary:self.deeplinkParameters forKey:@"params"];
+    [ADJPackageBuilder parameters:parameters setDate:self.clickTime                    forKey:@"click_time"];
+    [ADJPackageBuilder parameters:parameters setDate:self.purchaseTime                 forKey:@"purchase_time"];
 
     if (self.attribution != nil) {
-        [self parameters:parameters setString:self.attribution.trackerName  forKey:@"tracker"];
-        [self parameters:parameters setString:self.attribution.campaign     forKey:@"campaign"];
-        [self parameters:parameters setString:self.attribution.adgroup      forKey:@"adgroup"];
-        [self parameters:parameters setString:self.attribution.creative     forKey:@"creative"];
+        [ADJPackageBuilder parameters:parameters setString:self.attribution.trackerName  forKey:@"tracker"];
+        [ADJPackageBuilder parameters:parameters setString:self.attribution.campaign     forKey:@"campaign"];
+        [ADJPackageBuilder parameters:parameters setString:self.attribution.adgroup      forKey:@"adgroup"];
+        [ADJPackageBuilder parameters:parameters setString:self.attribution.creative     forKey:@"creative"];
     }
-
-    [self parameters:parameters setDictionary:self.iadDetails   forKey:@"details"];
-    [self parameters:parameters setString:self.deeplink         forKey:@"deeplink"];
-    [self parameters:parameters setString:self.deviceToken      forKey:@"push_token"];
+    [ADJPackageBuilder parameters:parameters setDictionary:self.iadDetails forKey:@"details"];
+    [ADJPackageBuilder parameters:parameters setString:self.deeplink forKey:@"deeplink"];
+    [ADJPackageBuilder parameters:parameters setString:self.deviceToken forKey:@"push_token"];
 
     ADJActivityPackage *clickPackage = [self defaultActivityPackage];
     clickPackage.path = @"/sdk_click";
@@ -162,61 +157,65 @@
     return parameters;
 }
 
-- (void)injectDeviceInfoIds:(ADJDeviceInfo *)deviceInfo
-             intoParameters:(NSMutableDictionary *)parameters {
-    [self parameters:parameters setString:deviceInfo.idForAdvertisers  forKey:@"idfa"];
-    [self parameters:parameters setString:deviceInfo.vendorId          forKey:@"idfv"];
+- (void) injectDeviceInfoIds:(ADJDeviceInfo *)deviceInfo
+           intoParameters:(NSMutableDictionary *) parameters
+{
+    [ADJPackageBuilder parameters:parameters setString:deviceInfo.idForAdvertisers  forKey:@"idfa"];
+    [ADJPackageBuilder parameters:parameters setString:deviceInfo.vendorId          forKey:@"idfv"];
 }
 
-- (void)injectDeviceInfo:(ADJDeviceInfo *)deviceInfo
-          intoParameters:(NSMutableDictionary *)parameters {
-    [self injectDeviceInfoIds:deviceInfo intoParameters:parameters];
-
-    [self parameters:parameters setString:deviceInfo.fbAttributionId    forKey:@"fb_id"];
-    [self parameters:parameters setInt:deviceInfo.trackingEnabled       forKey:@"tracking_enabled"];
-    [self parameters:parameters setString:deviceInfo.bundeIdentifier    forKey:@"bundle_id"];
-    [self parameters:parameters setString:deviceInfo.bundleVersion      forKey:@"app_version"];
-    [self parameters:parameters setString:deviceInfo.bundleShortVersion forKey:@"app_version_short"];
-    [self parameters:parameters setString:deviceInfo.deviceType         forKey:@"device_type"];
-    [self parameters:parameters setString:deviceInfo.deviceName         forKey:@"device_name"];
-    [self parameters:parameters setString:deviceInfo.osName             forKey:@"os_name"];
-    [self parameters:parameters setString:deviceInfo.systemVersion      forKey:@"os_version"];
-    [self parameters:parameters setString:deviceInfo.languageCode       forKey:@"language"];
-    [self parameters:parameters setString:deviceInfo.countryCode        forKey:@"country"];
-    [self parameters:parameters setString:deviceInfo.machineModel       forKey:@"hardware_name"];
-    [self parameters:parameters setString:deviceInfo.cpuSubtype         forKey:@"cpu_type"];
+- (void) injectDeviceInfo:(ADJDeviceInfo *)deviceInfo
+           intoParameters:(NSMutableDictionary *) parameters
+{
+    [self injectDeviceInfoIds:deviceInfo
+               intoParameters:parameters];
+    [ADJPackageBuilder parameters:parameters setString:deviceInfo.fbAttributionId   forKey:@"fb_id"];
+    [ADJPackageBuilder parameters:parameters setInt:deviceInfo.trackingEnabled      forKey:@"tracking_enabled"];
+    [ADJPackageBuilder parameters:parameters setString:deviceInfo.bundeIdentifier   forKey:@"bundle_id"];
+    [ADJPackageBuilder parameters:parameters setString:deviceInfo.bundleVersion     forKey:@"app_version"];
+    [ADJPackageBuilder parameters:parameters setString:deviceInfo.bundleShortVersion forKey:@"app_version_short"];
+    [ADJPackageBuilder parameters:parameters setString:deviceInfo.deviceType        forKey:@"device_type"];
+    [ADJPackageBuilder parameters:parameters setString:deviceInfo.deviceName        forKey:@"device_name"];
+    [ADJPackageBuilder parameters:parameters setString:deviceInfo.osName            forKey:@"os_name"];
+    [ADJPackageBuilder parameters:parameters setString:deviceInfo.systemVersion     forKey:@"os_version"];
+    [ADJPackageBuilder parameters:parameters setString:deviceInfo.languageCode      forKey:@"language"];
+    [ADJPackageBuilder parameters:parameters setString:deviceInfo.countryCode       forKey:@"country"];
+    [ADJPackageBuilder parameters:parameters setString:deviceInfo.machineModel      forKey:@"hardware_name"];
+    [ADJPackageBuilder parameters:parameters setString:deviceInfo.cpuSubtype       forKey:@"cpu_type"];
 }
 
-- (void)injectConfig:(ADJConfig*)adjustConfig
-      intoParameters:(NSMutableDictionary *)parameters {
-    [self parameters:parameters setString:adjustConfig.appToken             forKey:@"app_token"];
-    [self parameters:parameters setString:adjustConfig.environment          forKey:@"environment"];
-    [self parameters:parameters setBool:adjustConfig.hasResponseDelegate    forKey:@"needs_response_details"];
-    [self parameters:parameters setBool:adjustConfig.eventBufferingEnabled  forKey:@"event_buffering_enabled"];
+- (void)injectConfig:(ADJConfig*) adjustConfig
+       intoParameters:(NSMutableDictionary *) parameters
+{
+    [ADJPackageBuilder parameters:parameters setString:adjustConfig.appToken        forKey:@"app_token"];
+    [ADJPackageBuilder parameters:parameters setString:adjustConfig.environment     forKey:@"environment"];
+    [ADJPackageBuilder parameters:parameters setBool:adjustConfig.hasResponseDelegate forKey:@"needs_response_details"];
+    [ADJPackageBuilder parameters:parameters setBool:adjustConfig.eventBufferingEnabled forKey:@"event_buffering_enabled"];
 }
 
-- (void)injectActivityState:(ADJActivityState *)activityState
-              intoParamters:(NSMutableDictionary *)parameters {
-    [self parameters:parameters setInt:activityState.sessionCount       forKey:@"session_count"];
-    [self parameters:parameters setInt:activityState.subsessionCount    forKey:@"subsession_count"];
-    [self parameters:parameters setDuration:activityState.sessionLength forKey:@"session_length"];
-    [self parameters:parameters setDuration:activityState.timeSpent     forKey:@"time_spent"];
+- (void) injectActivityState:(ADJActivityState *)activityState
+               intoParamters:(NSMutableDictionary *)parameters {
+    [ADJPackageBuilder parameters:parameters setInt:activityState.sessionCount       forKey:@"session_count"];
+    [ADJPackageBuilder parameters:parameters setInt:activityState.subsessionCount    forKey:@"subsession_count"];
+    [ADJPackageBuilder parameters:parameters setDuration:activityState.sessionLength forKey:@"session_length"];
+    [ADJPackageBuilder parameters:parameters setDuration:activityState.timeSpent     forKey:@"time_spent"];
+    [ADJPackageBuilder parameters:parameters setString:activityState.uuid            forKey:@"ios_uuid"];
 
     // Check if UUID was persisted or not.
     // If yes, assign it to persistent_ios_uuid parameter.
     // If not, assign it to ios_uuid parameter.
     if (activityState.isPersisted) {
-        [self parameters:parameters setString:activityState.uuid        forKey:@"persistent_ios_uuid"];
+       [ADJPackageBuilder parameters:parameters setString:activityState.uuid        forKey:@"persistent_ios_uuid"];
     } else {
-        [self parameters:parameters setString:activityState.uuid        forKey:@"ios_uuid"];
+       [ADJPackageBuilder parameters:parameters setString:activityState.uuid        forKey:@"ios_uuid"];
     }
-
 
 }
 
-- (void)injectCreatedAt:(double)createdAt
-         intoParameters:(NSMutableDictionary *)parameters {
-    [self parameters:parameters setDate1970:createdAt forKey:@"created_at"];
+- (void)injectCreatedAt:(double) createdAt
+      intoParameters:(NSMutableDictionary *) parameters
+{
+    [ADJPackageBuilder parameters:parameters setDate1970:createdAt forKey:@"created_at"];
 }
 
 - (NSString *)eventSuffix:(ADJEvent *)event {
@@ -227,94 +226,75 @@
     }
 }
 
-- (void)parameters:(NSMutableDictionary *)parameters setString:(NSString *)value forKey:(NSString *)key {
-    if (value == nil || [value isEqualToString:@""]) {
-        return;
-    }
++ (void)parameters:(NSMutableDictionary *)parameters setString:(NSString *)value forKey:(NSString *)key {
+    if (value == nil || [value isEqualToString:@""]) return;
 
     [parameters setObject:value forKey:key];
 }
 
-- (void)parameters:(NSMutableDictionary *)parameters setInt:(int)value forKey:(NSString *)key {
-    if (value < 0) {
-        return;
-    }
++ (void)parameters:(NSMutableDictionary *)parameters setInt:(int)value forKey:(NSString *)key {
+    if (value < 0) return;
 
     NSString *valueString = [NSString stringWithFormat:@"%d", value];
-    [self parameters:parameters setString:valueString forKey:key];
+    [ADJPackageBuilder parameters:parameters setString:valueString forKey:key];
 }
 
-- (void)parameters:(NSMutableDictionary *)parameters setDate1970:(double)value forKey:(NSString *)key {
-    if (value < 0) {
-        return;
-    }
++ (void)parameters:(NSMutableDictionary *)parameters setDate1970:(double)value forKey:(NSString *)key {
+    if (value < 0) return;
 
     NSString *dateString = [ADJUtil formatSeconds1970:value];
-    [self parameters:parameters setString:dateString forKey:key];
+    [ADJPackageBuilder parameters:parameters setString:dateString forKey:key];
 }
 
-- (void)parameters:(NSMutableDictionary *)parameters setDate:(NSDate *)value forKey:(NSString *)key {
-    if (value == nil) {
-        return;
-    }
++ (void)parameters:(NSMutableDictionary *)parameters setDate:(NSDate *)value forKey:(NSString *)key {
+    if (value == nil) return;
 
     NSString *dateString = [ADJUtil formatDate:value];
-    [self parameters:parameters setString:dateString forKey:key];
+    [ADJPackageBuilder parameters:parameters setString:dateString forKey:key];
 }
 
-- (void)parameters:(NSMutableDictionary *)parameters setDuration:(double)value forKey:(NSString *)key {
-    if (value < 0) {
-        return;
-    }
++ (void)parameters:(NSMutableDictionary *)parameters setDuration:(double)value forKey:(NSString *)key {
+    if (value < 0) return;
 
     int intValue = round(value);
-    [self parameters:parameters setInt:intValue forKey:key];
+    [ADJPackageBuilder parameters:parameters setInt:intValue forKey:key];
 }
 
-- (void)parameters:(NSMutableDictionary *)parameters setDictionaryJson:(NSDictionary *)dictionary forKey:(NSString *)key {
-    if (dictionary == nil) {
-        return;
-    }
-
-    if (dictionary.count == 0) {
-        return;
-    }
-
-    if (![NSJSONSerialization isValidJSONObject:dictionary]) {
-        return;
-    }
++ (void)parameters:(NSMutableDictionary *)parameters setDictionaryJson:(NSDictionary *)dictionary forKey:(NSString *)key {
+    if (dictionary == nil) return;
+    if (dictionary.count == 0) return;
+    if (![NSJSONSerialization isValidJSONObject:dictionary]) return;
 
     NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dictionary options:0 error:nil];
     NSString *dictionaryString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-
-    [self parameters:parameters setString:dictionaryString forKey:key];
+    [ADJPackageBuilder parameters:parameters setString:dictionaryString forKey:key];
 }
 
-- (void)parameters:(NSMutableDictionary *)parameters setDictionary:(NSDictionary *)dictionary forKey:(NSString *)key {
-    if (dictionary == nil) {
-        return;
-    }
++ (void)parameters:(NSMutableDictionary *)parameters setDictionary:(NSDictionary *)dictionary forKey:(NSString *)key {
+    if (dictionary == nil) return;
+    if (dictionary.count == 0) return;
 
     if (dictionary.count == 0) {
         return;
     }
 
-    NSDictionary *convertedDictionary = [ADJUtil convertDictionaryValues:dictionary];
-    [self parameters:parameters setDictionaryJson:convertedDictionary forKey:key];
+    NSDictionary * convertedDictionary = [ADJUtil convertDictionaryValues:dictionary];
+
+    [ADJPackageBuilder parameters:parameters setDictionaryJson:convertedDictionary forKey:key];
 }
 
-- (void)parameters:(NSMutableDictionary *)parameters setBool:(BOOL)value forKey:(NSString *)key {
++ (void)parameters:(NSMutableDictionary *)parameters setBool:(BOOL)value forKey:(NSString *)key {
     int valueInt = [[NSNumber numberWithBool:value] intValue];
-    [self parameters:parameters setInt:valueInt forKey:key];
+
+    [ADJPackageBuilder parameters:parameters setInt:valueInt forKey:key];
 }
 
-- (void)parameters:(NSMutableDictionary *)parameters setNumber:(NSNumber *)value forKey:(NSString *)key {
-    if (value == nil) {
-        return;
-    }
++ (void)parameters:(NSMutableDictionary *)parameters setNumber:(NSNumber *)value forKey:(NSString *)key {
+    if (value == nil) return;
 
     NSString *numberString = [NSString stringWithFormat:@"%.5f", [value doubleValue]];
-    [self parameters:parameters setString:numberString forKey:key];
+
+    [ADJPackageBuilder parameters:parameters setString:numberString forKey:key];
 }
 
 @end
