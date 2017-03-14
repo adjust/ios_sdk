64637a18c61f7088c1276bdb2f716ac74168c713
uerceg <ugi@adjust.com>
2017-03-14 11:36:48 +0100

    Adding SHA1 hashing method to NSString category

--- Adjust/ADJAdditions/NSString+ADJAdditions.m
+++ Adjust/ADJAdditions/NSString+ADJAdditions.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2012-2014 adjust GmbH. All rights reserved.
 //
 
+#import <CommonCrypto/CommonDigest.h>
+
 #import "NSString+ADJAdditions.h"
 
 @implementation NSString(ADJAdditions)
@@ -41,6 +43,21 @@
     return [self stringByReplacingOccurrencesOfString:@":" withString:@""];
 }
 
+- (NSString *)adjSha1 {
+    NSData *data = [self dataUsingEncoding:NSUTF8StringEncoding];
+    uint8_t digest[CC_SHA1_DIGEST_LENGTH];
+
+    CC_SHA1(data.bytes, (CC_LONG)data.length, digest);
+
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
+
+    for (int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++) {
+        [output appendFormat:@"%02x", digest[i]];
+    }
+
+    return output;
+}
+
 + (NSString *)adjJoin:(NSString *)first, ... {
     NSString *iter, *result = first;
     va_list strings;
@@ -52,10 +69,11 @@
     }
     
     va_end(strings);
+
     return result;
 }
 
-+ (BOOL) adjIsEqual:(NSString *)first toString:(NSString *)second {
++ (BOOL)adjIsEqual:(NSString *)first toString:(NSString *)second {
     if (first == nil && second == nil) {
         return YES;
     }
