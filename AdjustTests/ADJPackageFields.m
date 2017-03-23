9cff50082ace49ad40fefba833692a78e29b5015
uerceg <ugi@adjust.com>
2017-03-23 12:06:56 +0100

    New version 4.11.3

--- AdjustTests/ADJPackageFields.m
+++ AdjustTests/ADJPackageFields.m
@@ -16,7 +16,7 @@
 
     // default values
     self.appToken = @"qwerty123456";
-    self.clientSdk = @"ios4.11.2";
+    self.clientSdk = @"ios4.11.3";
     self.suffix = @"";
     self.environment = @"sandbox";
 
