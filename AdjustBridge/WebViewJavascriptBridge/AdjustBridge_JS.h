09f754b72db9cf4fa5503f56a6557bb10858a04b
nonelse <pedro@adjust.com>
2018-07-20 11:57:27 +0200

    Save replace js with .m

---
+++ AdjustBridge/WebViewJavascriptBridge/AdjustBridge_JS.h
@@ -0,0 +1,11 @@
+//
+//  AdjustBridge_JS.h
+//  AdjustBridge
+//
+//  Created by Pedro on 20.07.18.
+//  Copyright © 2018 adjust GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NSString * AdjustBridge_js(void);
