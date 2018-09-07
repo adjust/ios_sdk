3e0ee996850bcdbc40ede89836c9da18b2bb3275
nonelse <pedro@adjust.com>
2018-09-07 15:00:59 +0200

    Create bridge test app

---
+++ AdjustTests/AdjustWebBridgeTestApp/AdjustWebBridgeTestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AdjustWebBridgeTestApp
+//
+//  Created by Pedro on 06.08.18.
+//  Copyright © 2018 adjust. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AdjustTests/AdjustWebBridgeTestApp/AdjustWebBridgeTestApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AdjustWebBridgeTestApp
+//
+//  Created by Pedro on 06.08.18.
+//  Copyright © 2018 adjust. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
