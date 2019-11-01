5d143563e66674f33346307a376b97d8e93badc1
Felipe Marino <felipemarino91@gmail.com>
2019-11-01 16:22:48 +0100

    Fix crash when valueDecoded is nil

--- Adjust/ADJActivityHandler.m
+++ Adjust/ADJActivityHandler.m
@@ -1337,6 +1337,7 @@ remainsPausedMessage:(NSString *)remainsPausedMessage
     if (value.length == 0) return NO;
 
     NSString* valueDecoded = [value adjUrlDecode];
+    if (!valueDecoded) return NO;
 
     NSString* keyWOutPrefix = [keyDecoded substringFromIndex:kAdjustPrefix.length];
     if (keyWOutPrefix.length == 0) return NO;
