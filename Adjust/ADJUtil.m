0bf89c5aea70d880d29ef96ac7424cabead8d422
uerceg <ugi@adjust.com>
2020-03-16 13:03:28 +0100

    New version 4.21.0

--- Adjust/ADJUtil.m
+++ Adjust/ADJUtil.m
@@ -40,7 +40,7 @@ static CTCarrier *carrier = nil;
 static CTTelephonyNetworkInfo *networkInfo = nil;
 #endif
 
-static NSString * const kClientSdk                  = @"ios4.20.0";
+static NSString * const kClientSdk                  = @"ios4.21.0";
 static NSString * const kDeeplinkParam              = @"deep_link=";
 static NSString * const kSchemeDelimiter            = @"://";
 static NSString * const kDefaultScheme              = @"AdjustUniversalScheme";
