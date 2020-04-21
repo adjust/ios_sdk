cad90dfdb4f7d9eb7f2da545ea7ac8fd2d8d1507
uerceg <ugi@adjust.com>
2020-04-21 02:55:23 +0200

    New version 4.21.3

--- Adjust/Adjust.h
+++ Adjust/Adjust.h
@@ -2,7 +2,7 @@
 //  Adjust.h
 //  Adjust
 //
-//  V4.21.2
+//  V4.21.3
 //  Created by Christian Wellenbrock (wellle) on 23rd July 2013.
 //  Copyright © 2012-2017 Adjust GmbH. All rights reserved.
 //
