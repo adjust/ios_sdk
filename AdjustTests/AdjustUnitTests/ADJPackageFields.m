1514f541f3a651c7c948e18cf95e7253d87680a9
rabc <ricardo.carvalho@adjust.com>
2021-02-04 15:52:27 +0100

    Change version to 4.26.1

--- AdjustTests/AdjustUnitTests/ADJPackageFields.m
+++ AdjustTests/AdjustUnitTests/ADJPackageFields.m
@@ -16,7 +16,7 @@
 
     // default values
     self.appToken = @"qwerty123456";
-    self.clientSdk = @"ios4.26.0";
+    self.clientSdk = @"ios4.26.1";
     self.suffix = @"";
     self.environment = @"sandbox";
 
