8fb86b86af1cac70622405b86a878b263e25acb7
uerceg <ugi@adjust.com>
2021-04-01 07:18:55 +0200

    update version number to 4.28.0

--- AdjustBridge/AdjustBridgeRegister.m
+++ AdjustBridge/AdjustBridgeRegister.m
@@ -246,7 +246,7 @@ static NSString * fbAppIdStatic = nil;
                 if (this.sdkPrefix) {
                     return this.sdkPrefix;
                 } else {
-                    return 'web-bridge4.27.1';
+                    return 'web-bridge4.28.0';
                 }
             },
             setTestOptions: function(testOptions) {
