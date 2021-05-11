c6824a3a0a7eae94f6c8a4ee12b99a661a65e030
rabc <ricardo.carvalho@adjust.com>
2021-05-11 13:52:33 +0200

    Convert to NSInvocation

--- Adjust/ADJUtil.m
+++ Adjust/ADJUtil.m
@@ -457,10 +457,16 @@ static NSString * const kDateFormat                 = @"yyyy-MM-dd'T'HH:mm:ss.SS
     if (![man respondsToSelector:selExi]) {
         return NO;
     }
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-    BOOL exists = (BOOL)[man performSelector:selExi withObject:path];
-#pragma clang diagnostic pop
+    
+    NSInvocation *invMan = [NSInvocation invocationWithMethodSignature:[man methodSignatureForSelector:selExi]];
+    [invMan setSelector:selExi];
+    [invMan setTarget:man];
+    [invMan setArgument:&path atIndex:2];
+    [invMan invoke];
+    
+    BOOL exists;
+    [invMan getReturnValue:&exists];
+    
     if (!exists) {
         [[ADJAdjustFactory logger] debug:@"%@ directory not present and will be created", folderName];
         BOOL withIntermediateDirectories = NO;
