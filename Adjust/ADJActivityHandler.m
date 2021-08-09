53a38c43062e78d2814705050c4f7ec683ce6df5
uerceg <ugi@adjust.com>
2021-08-09 22:27:38 +0200

    improve AdServices.framework integration logging a bit

--- Adjust/ADJActivityHandler.m
+++ Adjust/ADJActivityHandler.m
@@ -1767,6 +1767,9 @@ preLaunchActions:(ADJSavedPreLaunch*)preLaunchActions
     }
     
     // Fetch if no attribution OR not sent to backend yet
+    if ([ADJUserDefaults getAdServicesTracked]) {
+        [selfI.logger debug:@"AdServices attribution info already read"];
+    }
     return (selfI.attribution == nil || ![ADJUserDefaults getAdServicesTracked]);
 }
 
@@ -2739,7 +2742,7 @@ sdkClickHandlerOnly:(BOOL)sdkClickHandlerOnly
     
     Class skAdNetwork = NSClassFromString(@"SKAdNetwork");
     if (skAdNetwork == nil) {
-        [logger warn:@"StoreKit framework not found in user's app (SKAdNetwork not found)"];
+        [logger warn:@"StoreKit framework not found in the app (SKAdNetwork not found)"];
         return;
     }
     
