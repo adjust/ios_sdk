c1aa97d41f2319430690ed00e499e4e61b77673c
uerceg <ugi@adjust.com>
2022-08-22 15:41:04 +0200

    feat: add partner sharing options feature to web bridge

--- AdjustBridge/AdjustBridge.m
+++ AdjustBridge/AdjustBridge.m
@@ -537,20 +537,25 @@
     [self.bridgeRegister registerHandler:@"adjust_trackThirdPartySharing" handler:^(id data, WVJBResponseCallback responseCallback) {
         id isEnabledO = [data objectForKey:@"isEnabled"];
         id granularOptions = [data objectForKey:@"granularOptions"];
+        id partnerSharingSettings = [data objectForKey:@"partnerSharingSettings"];
 
         NSNumber *isEnabled = nil;
         if ([isEnabledO isKindOfClass:[NSNumber class]]) {
             isEnabled = (NSNumber *)isEnabledO;
         }
-
         ADJThirdPartySharing *adjustThirdPartySharing =
             [[ADJThirdPartySharing alloc] initWithIsEnabledNumberBool:isEnabled];
-
         for (int i = 0; i < [granularOptions count]; i += 3) {
             NSString *partnerName = [[granularOptions objectAtIndex:i] description];
             NSString *key = [[granularOptions objectAtIndex:(i + 1)] description];
             NSString *value = [[granularOptions objectAtIndex:(i + 2)] description];
             [adjustThirdPartySharing addGranularOption:partnerName key:key value:value];
+        }
+        for (int i = 0; i < [partnerSharingSettings count]; i += 3) {
+            NSString *partnerName = [[partnerSharingSettings objectAtIndex:i] description];
+            NSString *key = [[partnerSharingSettings objectAtIndex:(i + 1)] description];
+            BOOL value = [[partnerSharingSettings objectAtIndex:(i + 2)] boolValue];
+            [adjustThirdPartySharing addPartnerSharingSetting:partnerName key:key value:value];
         }
 
         [Adjust trackThirdPartySharing:adjustThirdPartySharing];
--- AdjustBridge/AdjustBridgeRegister.m
+++ AdjustBridge/AdjustBridgeRegister.m
@@ -300,12 +300,17 @@ static NSString * fbAppIdStatic = nil;
         window.AdjustThirdPartySharing = function(isEnabled) {
             this.isEnabled = isEnabled;
             this.granularOptions = [];
+            this.partnerSharingSettings = [];
         };
-
         AdjustThirdPartySharing.prototype.addGranularOption = function(partnerName, key, value) {
             this.granularOptions.push(partnerName);
             this.granularOptions.push(key);
             this.granularOptions.push(value);
+        };
+        AdjustThirdPartySharing.prototype.addPartnerSharingSetting = function(partnerName, key, value) {
+            this.partnerSharingSettings.push(partnerName);
+            this.partnerSharingSettings.push(key);
+            this.partnerSharingSettings.push(value);
         };
 
         // Copied from adjust_config.js
