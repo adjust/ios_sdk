958bdf4c5764c727944ebf3c6d8348d21cbf2214
uerceg <ugi@adjust.com>
2023-01-31 14:53:46 +0100

    refac: remove iAd handling from native command executor

--- AdjustTests/AdjustTestApp/AdjustTestApp/ATAAdjustCommandExecutor.m
+++ AdjustTests/AdjustTestApp/AdjustTestApp/ATAAdjustCommandExecutor.m
@@ -136,13 +136,6 @@
             testOptions.noBackoffWait = YES;
         }
     }
-    testOptions.iAdFrameworkEnabled = NO; // default value -> NO - iAd will not be used in test app by default
-    if ([parameters objectForKey:@"iAdFrameworkEnabled"]) {
-        NSString *iAdFrameworkEnabledStr = [parameters objectForKey:@"iAdFrameworkEnabled"][0];
-        if ([iAdFrameworkEnabledStr isEqualToString:@"true"]) {
-            testOptions.iAdFrameworkEnabled = YES;
-        }
-    }
     testOptions.adServicesFrameworkEnabled = NO; // default value -> NO - AdServices will not be used in test app by default
     if ([parameters objectForKey:@"adServicesFrameworkEnabled"]) {
         NSString *adServicesFrameworkEnabledStr = [parameters objectForKey:@"adServicesFrameworkEnabled"][0];
