0bc650f40cd738fce9531b41b7faec835878f089
uerceg <ugi@adjust.com>
2023-06-08 17:57:26 +0200

    feat: update version number to 4.34.0

--- AdjustBridge/AdjustBridgeRegister.m
+++ AdjustBridge/AdjustBridgeRegister.m
@@ -275,7 +275,7 @@ static NSString * fbAppIdStatic = nil;
                 if (this.sdkPrefix) {
                     return this.sdkPrefix;
                 } else {
-                    return 'web-bridge4.33.6';
+                    return 'web-bridge4.34.0';
                 }
             },
             setTestOptions: function(testOptions) {
