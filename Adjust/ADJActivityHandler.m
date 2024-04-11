fe15f03a20c6855cb14e2ae1651cee92a88e88a4
uerceg <ugi@adjust.com>
2024-04-11 10:52:09 +0200

    refac: refer to multiple prameters

--- Adjust/ADJActivityHandler.m
+++ Adjust/ADJActivityHandler.m
@@ -746,8 +746,8 @@ const NSUInteger kWaitingForAttStatusLimitSeconds = 120;
 + (void)deleteState {
     [ADJActivityHandler deleteActivityState];
     [ADJActivityHandler deleteAttribution];
-    [ADJActivityHandler deleteGlobalCallbackParameter];
-    [ADJActivityHandler deleteGlobalPartnerParameter];
+    [ADJActivityHandler deleteGlobalCallbackParameters];
+    [ADJActivityHandler deleteGlobalPartnerParameters];
     [ADJUserDefaults clearAdjustStuff];
 }
 
@@ -759,11 +759,11 @@ const NSUInteger kWaitingForAttStatusLimitSeconds = 120;
     [ADJUtil deleteFileWithName:kAttributionFilename];
 }
 
-+ (void)deleteGlobalCallbackParameter {
++ (void)deleteGlobalCallbackParameters {
     [ADJUtil deleteFileWithName:kGlobalCallbackParametersFilename];
 }
 
-+ (void)deleteGlobalPartnerParameter {
++ (void)deleteGlobalPartnerParameters {
     [ADJUtil deleteFileWithName:kGlobalPartnerParametersFilename];
 }
 
