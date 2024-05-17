cc6af0fa70eae4dca1ed5cad01f6112533d07b78
uerceg <ugi@adjust.com>
2024-05-17 17:29:52 +0200

    refac: rename trackSubscription: to trackAppStoreSubscription:

--- Adjust/ADJActivityHandler.h
+++ Adjust/ADJActivityHandler.h
@@ -110,7 +110,7 @@
 
 - (void)trackThirdPartySharing:(nonnull ADJThirdPartySharing *)thirdPartySharing;
 - (void)trackMeasurementConsent:(BOOL)enabled;
-- (void)trackSubscription:(ADJAppStoreSubscription * _Nullable)subscription;
+- (void)trackAppStoreSubscription:(ADJAppStoreSubscription * _Nullable)subscription;
 - (void)updateAttStatusFromUserCallback:(int)newAttStatusFromUser;
 - (void)trackAdRevenue:(ADJAdRevenue * _Nullable)adRevenue;
 - (void)verifyPurchase:(nonnull ADJPurchase *)purchase
