0db562a93d753700db227455f7f3e6689011071c
nonelse <pedro@adjust.com>
2024-05-24 14:54:09 +0200

    Refac: Test networking

--- AdjustTests/AdjustTestLibrary/AdjustTestLibrary/ATLNetworking.m
+++ AdjustTests/AdjustTestLibrary/AdjustTestLibrary/ATLNetworking.m
@@ -1,131 +1,135 @@
 //
-//  ATLUtilNetworking.m
+//  ATLNetworking.m
 //  AdjustTestLibrary
 //
-//  Created by Pedro on 18.04.17.
-//  Copyright © 2017 adjust. All rights reserved.
+//  Created by Pedro Silva on 24.05.24.
+//  Copyright © 2024 adjust. All rights reserved.
 //
 
-#import "ATLUtilNetworking.h"
+#import "ATLNetworking.h"
 #import "ATLUtil.h"
-#import "ATLTestLibrary.h"
 
 static const double kRequestTimeout = 60;   // 60 seconds
-static NSURLSessionConfiguration *urlSessionConfiguration = nil;
 
 @implementation ATLHttpResponse
 @end
 
 @implementation ATLHttpRequest
+- (nonnull id)initWithPath:(nonnull NSString *)path
+                      base:(nullable NSString *)base
+{
+    self = [super init];
+    _path = path;
+    _base = base;
+
+    return self;
+}
+
 @end
 
-@implementation ATLUtilNetworking
+@interface ATLNetworking ()
 
-+ (void)initialize {
-    if (self != [ATLUtilNetworking class]) {
+@property (nonatomic, nonnull, strong) NSURLSessionConfiguration *urlSessionConfiguration;
+
+@end
+
+@implementation ATLNetworking
+
+- (nonnull instancetype)init {
+    self = [super init];
+    _urlSessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+
+    return self;
+}
+
+- (void)sendPostRequestWithData:(nonnull ATLHttpRequest *)requestData
+                        baseUrl:(nonnull NSURL *)baseUrl
+                responseHandler:(nonnull httpResponseHandler)responseHandler
+{
+    NSMutableURLRequest *_Nullable request = [self requestWithData:requestData
+                                                            baseUrl:baseUrl];
+    if (request == nil) {
         return;
     }
-    
-    [self initializeUrlSessionConfiguration];
-}
 
-+ (void)initializeUrlSessionConfiguration {
-    urlSessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
-}
+    Class NSURLSessionClass = NSClassFromString(@"NSURLSession");
 
-+ (NSString *)appendBasePath:(NSString *)basePath
-                        path:(NSString *)path {
-    if (basePath == nil) {
-        return path;
+    if (NSURLSessionClass != nil) {
+        [self sendNSURLSessionRequest:request
+                      responseHandler:responseHandler];
+    } else {
+        [self sendNSURLConnectionRequest:request
+                         responseHandler:responseHandler];
     }
-    return [NSString stringWithFormat:@"%@%@", basePath, path];
 }
 
-+ (void)sendPostRequest:(ATLHttpRequest *)requestData
-        responseHandler:(httpResponseHandler)responseHandler
+- (nullable NSMutableURLRequest *)requestWithData:(nonnull ATLHttpRequest *)requestData
+                                          baseUrl:(nonnull NSURL *)baseUrl
 {
-    NSMutableURLRequest *request = [ATLUtilNetworking requestForPackage:requestData];
-    
-    [ATLUtilNetworking sendRequest:request
-                   responseHandler:responseHandler];
-}
+    NSString *_Nonnull mergedPath = requestData.base == nil ?
+        requestData.path : [NSString stringWithFormat:@"%@%@", requestData.base, requestData.path];
 
-+ (NSMutableURLRequest *)requestForPackage:(ATLHttpRequest *)requestData
-{
-    NSURL *url = [NSURL URLWithString:requestData.path relativeToURL:[ATLTestLibrary baseUrl]];
+    NSURL *_Nullable url = [NSURL URLWithString:mergedPath relativeToURL:baseUrl];
+    if (url == nil) {
+        return nil;
+    }
+
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
     request.timeoutInterval = kRequestTimeout;
     request.HTTPMethod = @"POST";
-    
+
     [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
-    
+
     if (requestData.headerFields != nil) {
         for (NSString *key in requestData.headerFields) {
             [request setValue:requestData.headerFields[key] forHTTPHeaderField:key];
         }
     }
-    
+
     if (requestData.bodyString != nil) {
         NSData *body = [NSData dataWithBytes:requestData.bodyString.UTF8String length:requestData.bodyString.length];
         [request setHTTPBody:body];
     }
-    
+
     return request;
 }
 
-+ (void)sendRequest:(NSMutableURLRequest *)request
-    responseHandler:(httpResponseHandler)responseHandler
+- (void)sendNSURLSessionRequest:(nonnull NSMutableURLRequest *)request
+                responseHandler:(nonnull httpResponseHandler)responseHandler
 {
-    Class NSURLSessionClass = NSClassFromString(@"NSURLSession");
-   
-    if (NSURLSessionClass != nil) {
-        [ATLUtilNetworking sendNSURLSessionRequest:request
-                     responseHandler:responseHandler];
-    } else {
-        [ATLUtilNetworking sendNSURLConnectionRequest:request
-                        responseHandler:responseHandler];
-    }
-}
-
-+ (void)sendNSURLSessionRequest:(NSMutableURLRequest *)request
-            responseHandler:(httpResponseHandler)responseHandler
-{
-    NSURLSession *session = [NSURLSession sessionWithConfiguration:urlSessionConfiguration
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:self.urlSessionConfiguration
                                                 delegate:nil
                                            delegateQueue:nil];
-    
+
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request
                                             completionHandler:
                                   ^(NSData *data, NSURLResponse *response, NSError *error) {
-                                      ATLHttpResponse *httpResponseData =
-                                      [ATLUtilNetworking completionHandler:data
-                                                                   response:(NSHTTPURLResponse *)response
-                                                                      error:error];
-                                      responseHandler(httpResponseData);
+                                      responseHandler([ATLNetworking
+                                                       completionHandler:data
+                                                       response:(NSHTTPURLResponse *)response
+                                                       error:error]);
                                   }];
-    
+
     [task resume];
     [session finishTasksAndInvalidate];
 }
 
-+ (void)sendNSURLConnectionRequest:(NSMutableURLRequest *)request
+- (void)sendNSURLConnectionRequest:(NSMutableURLRequest *)request
                    responseHandler:(httpResponseHandler)responseHandler
 {
     NSError *responseError = nil;
     NSHTTPURLResponse *urlResponse = nil;
-    
+
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
     NSData *data = [NSURLConnection sendSynchronousRequest:request
                                          returningResponse:&urlResponse
                                                      error:&responseError];
 #pragma clang diagnostic pop
-    
-    ATLHttpResponse *httpResponseData = [ATLUtilNetworking completionHandler:data
-                                                                     response:(NSHTTPURLResponse *)urlResponse
-                                                                        error:responseError];
-    
-    responseHandler(httpResponseData);
+
+    responseHandler([ATLNetworking completionHandler:data
+                                            response:(NSHTTPURLResponse *)urlResponse
+                                               error:responseError]);
 }
 
 + (ATLHttpResponse *)completionHandler:(NSData *)data
@@ -133,20 +137,20 @@ static NSURLSessionConfiguration *urlSessionConfiguration = nil;
                                  error:(NSError *)responseError
 {
     ATLHttpResponse *httpResponseData = [[ATLHttpResponse alloc] init];
-    
+
     // Connection error
     if (responseError != nil) {
         [ATLUtil debug:@"responseError %@", responseError.localizedDescription];
-        
+
         return httpResponseData;
     }
-    
+
     if ([ATLUtil isNull:data]) {
         [ATLUtil debug:@"data is null %@"];
 
         return httpResponseData;
     }
-    
+
     httpResponseData.responseString = [ATLUtil adjTrim:[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]];
     [ATLUtil debug:@"Response: %@", httpResponseData.responseString];
 
@@ -155,32 +159,34 @@ static NSURLSessionConfiguration *urlSessionConfiguration = nil;
     httpResponseData.headerFields = urlResponse.allHeaderFields;
     [ATLUtil debug:@"header fields: %@", httpResponseData.headerFields];
 
-    httpResponseData.jsonFoundation = [ATLUtilNetworking saveJsonResponse:data];
+    httpResponseData.jsonFoundation = [ATLNetworking saveJsonResponse:data];
     [ATLUtil debug:@"json response: %@", httpResponseData.jsonFoundation];
 
     [ATLUtil debug:@"json response class: %@", NSStringFromClass([httpResponseData.jsonFoundation class])];
     //2const char * cStringClassName = object_getClassName(httpResponseData.jsonFoundation);
-    
+
     return httpResponseData;
 }
 
 + (id)saveJsonResponse:(NSData *)jsonData {
     NSError *error = nil;
     NSException *exception = nil;
-    id jsonFoundation = [ATLUtilNetworking buildJsonFoundation:jsonData exceptionPtr:&exception errorPtr:&error];
-    
+    id jsonFoundation = [ATLNetworking buildJsonFoundation:jsonData
+                                              exceptionPtr:&exception
+                                                  errorPtr:&error];
+
     if (exception != nil) {
         [ATLUtil debug:@"Failed to parse json response. (%@)", exception.description];
-        
+
         return nil;
     }
-    
+
     if (error != nil) {
         [ATLUtil debug:@"Failed to parse json response. (%@)", error.description];
 
         return nil;
     }
-    
+
     return jsonFoundation;
 }
 
@@ -190,16 +196,16 @@ static NSURLSessionConfiguration *urlSessionConfiguration = nil;
     if (jsonData == nil) {
         return nil;
     }
-    
+
     id jsonFoundation = nil;
-    
+
     @try {
         jsonFoundation = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:error];
     } @catch (NSException *ex) {
         *exceptionPtr = ex;
         return nil;
     }
-    
+
     return jsonFoundation;
 }
 
