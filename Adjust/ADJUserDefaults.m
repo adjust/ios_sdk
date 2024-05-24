c0528021124254edc68c4f65f8b1e540c2ccadc0
Uglješa Erceg <ugi@adjust.com>
2024-05-24 03:26:59 -0400

    refac: rename user defaults key

--- Adjust/ADJUserDefaults.m
+++ Adjust/ADJUserDefaults.m
@@ -20,7 +20,7 @@ static NSString * const PREFS_KEY_SKAD_REGISTER_CALL_TIME = @"adj_skad_register_
 static NSString * const PREFS_KEY_LINK_ME_CHECKED = @"adj_link_me_checked";
 static NSString * const PREFS_KEY_DEEPLINK_URL_CACHED = @"adj_deeplink_url_cached";
 static NSString * const PREFS_KEY_ATT_WAITING_REMAINING_SECONDS = @"adj_att_waiting_remaining_seconds";
-static NSString * const PREFS_KEY_CONTROL_PARAMS = @"adj_att_control_params";
+static NSString * const PREFS_KEY_CONTROL_PARAMS = @"adj_control_params";
 
 @implementation ADJUserDefaults
 
