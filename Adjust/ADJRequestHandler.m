34accbaf7aef41009379a8a52ce151e32de6e410
uerceg <ugi@adjust.com>
2024-05-27 14:45:55 +0200

    feat: add beta version information into http header

--- Adjust/ADJRequestHandler.m
+++ Adjust/ADJRequestHandler.m
@@ -393,6 +393,7 @@ authorizationHeader:(NSString *)authorizationHeader
     request.HTTPMethod = @"POST";
     [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
     [request setValue:clientSdk forHTTPHeaderField:@"Client-Sdk"];
+    [request setValue:@"1" forHTTPHeaderField:@"Beta-Version"];
 
     NSUInteger sendingParametersCount = sendingParameters? sendingParameters.count : 0;
     NSMutableArray<NSString *> *kvParameters =
@@ -444,6 +445,7 @@ authorizationHeader:(NSString *)authorizationHeader
     request.timeoutInterval = self.requestTimeout;
     request.HTTPMethod = @"GET";
     [request setValue:clientSdk forHTTPHeaderField:@"Client-Sdk"];
+    [request setValue:@"1" forHTTPHeaderField:@"Beta-Version"];
     return request;
 }
 
