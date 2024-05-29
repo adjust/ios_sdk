68fc4cffbea4854d189dd436ae6cc1e52bc376b1
Aditi3 <Aditi3@users.noreply.github.com>
2024-05-29 13:17:53 +0200

    build: rename tv and imessage targets to AdjustSdk

---
+++ AdjustSdkIm/AdjustSdk.h
@@ -0,0 +1,33 @@
+//
+//  AdjustSdk.h
+//  AdjustSdkIm
+//
+//  Created by Aditi Agrawal on 28/05/24.
+//  Copyright © 2024 Adjust GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AdjustSdk.
+FOUNDATION_EXPORT double AdjustSdkVersionNumber;
+
+//! Project version string for AdjustSdk.
+FOUNDATION_EXPORT const unsigned char AdjustSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdjustSdk/PublicHeader.h>
+
+#import <AdjustSdk/Adjust.h>
+#import <AdjustSdk/ADJEvent.h>
+#import <AdjustSdk/ADJThirdPartySharing.h>
+#import <AdjustSdk/ADJConfig.h>
+#import <AdjustSdk/ADJLogger.h>
+#import <AdjustSdk/ADJAttribution.h>
+#import <AdjustSdk/ADJAppStoreSubscription.h>
+#import <AdjustSdk/ADJEventSuccess.h>
+#import <AdjustSdk/ADJEventFailure.h>
+#import <AdjustSdk/ADJSessionSuccess.h>
+#import <AdjustSdk/ADJSessionFailure.h>
+#import <AdjustSdk/ADJAdRevenue.h>
+#import <AdjustSdk/ADJLinkResolution.h>
+#import <AdjustSdk/ADJPurchase.h>
+#import <AdjustSdk/ADJPurchaseVerificationResult.h>
