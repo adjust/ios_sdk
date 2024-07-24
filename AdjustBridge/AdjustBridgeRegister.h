a664cbfda22de83ddf5a7f119a8c5664200d875f
Aditi3 <Aditi3@users.noreply.github.com>
2024-07-24 09:31:36 +0200

    feat: inject javascript via obj-c class

---
+++ AdjustBridge/AdjustBridgeRegister.h
@@ -0,0 +1,19 @@
+//
+//  AdjustBridgeRegister.h
+//  Adjust
+//
+//  Created by Aditi Agrawal on 22/07/24.
+//  Copyright © 2024 Adjust GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AdjustBridgeRegister : NSObject
+
++ (NSString *)AdjustBridge_js;
+
+@end
+
+NS_ASSUME_NONNULL_END
