ce4fa288f7e11f6ae02416e96d2f33d6bf5b4e88
genadyb <genady.buchatsky@gmail.com>
2025-01-10 15:51:15 +0100

    chore: version bump to 5.0.2

--- AdjustBridge/AdjustBridgeRegister.m
+++ AdjustBridge/AdjustBridgeRegister.m
@@ -176,7 +176,7 @@ static NSString * fbAppIdStatic = nil;
             if (this.sdkPrefix) {
                 return this.sdkPrefix;
             } else {
-                return 'web-bridge5.0.1';
+                return 'web-bridge5.0.2';
             }
         },
 
