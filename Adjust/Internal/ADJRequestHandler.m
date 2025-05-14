5831931a08d2c61f4b76d10580adce7a0870baf9
genadyb <genady.buchatsky@gmail.com>
2025-05-15 00:53:04 +0200

    chore: adds Beta-Version header

--- Adjust/Internal/ADJRequestHandler.m
+++ Adjust/Internal/ADJRequestHandler.m
@@ -449,7 +449,7 @@ authorizationHeader:(NSString *)authorizationHeader
     [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
     [request setValue:clientSdk forHTTPHeaderField:@"Client-Sdk"];
     // in case of beta release, specify build version here
-    // [request setValue:@"2" forHTTPHeaderField:@"Beta-Version"];
+    [request setValue:@"1" forHTTPHeaderField:@"Beta-Version"];
 
     NSMutableArray<NSString *> *kvParameters =
         [NSMutableArray arrayWithCapacity:mergedParameters.count];
