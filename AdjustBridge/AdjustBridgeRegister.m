8a74b8fb5bef3f8e541b99fd3146e9d412fe5466
uerceg <ugi@adjust.com>
2025-08-11 12:40:50 +0200

    update version number to 5.4.3

--- AdjustBridge/AdjustBridgeRegister.m
+++ AdjustBridge/AdjustBridgeRegister.m
@@ -176,7 +176,7 @@ static NSString * fbAppIdStatic = nil;
                 if (this.sdkPrefix) {
                     return this.sdkPrefix;
                 } else {
-                    return 'web-bridge5.4.2';
+                    return 'web-bridge5.4.3';
                 }
             },
 
