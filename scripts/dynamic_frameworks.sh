11d44687083b73196d35068d1fa911104d5e6a9b
genadyb <genady.buchatsky@gmail.com>
2026-02-19 19:35:01 +0100

    chore: readability improvements

--- scripts/dynamic_frameworks.sh
+++ scripts/dynamic_frameworks.sh
@@ -9,11 +9,12 @@ echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} Dynamic Frameworks build - START... ${N
 if [[ $BUILD_TARGET_IOS -eq 1 ]]
 then
   TEMP_OUTPUT_FOLDER="${XCF_OUTPUT_ROOT_FOLDER}/${XCF_OUTPUT_DYNAMIC_FRMKS_FOLDER}/${XCF_OUTPUT_FRMK_IOS_FOLDER_DYNAMIC}-framework"
+  ARCHIVE_FILE_NAME="${XCF_OUTPUT_FRMK_IOS_FOLDER_DYNAMIC}-framework-${SDK_VERSION}.zip"
   mkdir -p "${TEMP_OUTPUT_FOLDER}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =${NC}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} XCFramework: Building Dynamic Frameworks for iOS...${NC}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =${NC}"
-  build_dynamic_fat_framework "${SCHEMA_NAME__ADJUST_IOS}" "ios" "${XCF_FRM_NAME__ADJUST_IOS}" "${XCF_OUTPUT_ROOT_FOLDER}" "${TEMP_OUTPUT_FOLDER}" "${XCF_OUTPUT_FRMK_IOS_FOLDER_DYNAMIC}-framework-${SDK_VERSION}.zip"
+  build_dynamic_fat_framework "${SCHEMA_NAME__ADJUST_IOS}" "ios" "${XCF_FRM_NAME__ADJUST_IOS}" "${XCF_OUTPUT_ROOT_FOLDER}" "${TEMP_OUTPUT_FOLDER}" "${ARCHIVE_FILE_NAME}"
 else
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} Skipping iOS SDK Dynamic Framework build ... ${NC}"
 fi
@@ -24,11 +25,12 @@ fi
 if [[ $BUILD_TARGET_TVOS -eq 1 ]]
 then
   TEMP_OUTPUT_FOLDER="${XCF_OUTPUT_ROOT_FOLDER}/${XCF_OUTPUT_DYNAMIC_FRMKS_FOLDER}/${XCF_OUTPUT_FRMK_TV_FOLDER_DYNAMIC}-framework"
+  ARCHIVE_FILE_NAME="${XCF_OUTPUT_FRMK_TV_FOLDER_DYNAMIC}-framework-${SDK_VERSION}.zip"
   mkdir -p "${TEMP_OUTPUT_FOLDER}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =${NC}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} XCFramework: Building Dynamic Frameworks for tvOS...${NC}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =${NC}"
-  build_dynamic_fat_framework "${SCHEMA_NAME__ADJUST_TV}" "tvos" "${XCF_FRM_NAME__ADJUST_TV}" "${XCF_OUTPUT_ROOT_FOLDER}" "${TEMP_OUTPUT_FOLDER}" "${XCF_OUTPUT_FRMK_TV_FOLDER_DYNAMIC}-framework-${SDK_VERSION}.zip"
+  build_dynamic_fat_framework "${SCHEMA_NAME__ADJUST_TV}" "tvos" "${XCF_FRM_NAME__ADJUST_TV}" "${XCF_OUTPUT_ROOT_FOLDER}" "${TEMP_OUTPUT_FOLDER}" "${ARCHIVE_FILE_NAME}"
 else
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} Skipping tvOS SDK Dynamic Framework build ... ${NC}"
 fi
@@ -38,11 +40,12 @@ fi
 if [[ $BUILD_TARGET_IM -eq 1 ]]
 then
   TEMP_OUTPUT_FOLDER="${XCF_OUTPUT_ROOT_FOLDER}/${XCF_OUTPUT_DYNAMIC_FRMKS_FOLDER}/${XCF_OUTPUT_FRMK_IM_FOLDER_DYNAMIC}-framework"
+  ARCHIVE_FILE_NAME="${XCF_OUTPUT_FRMK_IM_FOLDER_DYNAMIC}-framework-${SDK_VERSION}.zip"
   mkdir -p "${TEMP_OUTPUT_FOLDER}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =${NC}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} XCFramework: Building Dynamic Frameworks for iOS (iMessage)...${NC}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =${NC}"
-  build_dynamic_fat_framework "${SCHEMA_NAME__ADJUST_IM}" "ios" "${XCF_FRM_NAME__ADJUST_IM}" "${XCF_OUTPUT_ROOT_FOLDER}" "${TEMP_OUTPUT_FOLDER}" "${XCF_OUTPUT_FRMK_IM_FOLDER_DYNAMIC}-framework-${SDK_VERSION}.zip"
+  build_dynamic_fat_framework "${SCHEMA_NAME__ADJUST_IM}" "ios" "${XCF_FRM_NAME__ADJUST_IM}" "${XCF_OUTPUT_ROOT_FOLDER}" "${TEMP_OUTPUT_FOLDER}" "${ARCHIVE_FILE_NAME}"
 else
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} Skipping iOS (iMessage) SDK Dynamic Framework build ... ${NC}"
 fi
@@ -52,11 +55,12 @@ fi
 if [[ $BUILD_TARGET_WEB_BRIDGE -eq 1 ]]
 then
   TEMP_OUTPUT_FOLDER="${XCF_OUTPUT_ROOT_FOLDER}/${XCF_OUTPUT_DYNAMIC_FRMKS_FOLDER}/${XCF_OUTPUT_FRMK_WEB_BRIDGE_FOLDER_DYNAMIC}-framework"
+  ARCHIVE_FILE_NAME="${XCF_OUTPUT_FRMK_WEB_BRIDGE_FOLDER_DYNAMIC}-framework-${SDK_VERSION}.zip"
   mkdir -p "${TEMP_OUTPUT_FOLDER}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =${NC}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} XCFramework: Building Dynamic Frameworks for iOS (WebBridge)...${NC}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =${NC}"
-  build_dynamic_fat_framework "${SCHEMA_NAME__ADJUST_WEB_BRIDGE}" "ios" "${XCF_FRM_NAME__ADJUST_WEB_BRIDGE}" "${XCF_OUTPUT_ROOT_FOLDER}" "${TEMP_OUTPUT_FOLDER}" "${XCF_OUTPUT_FRMK_WEB_BRIDGE_FOLDER_DYNAMIC}-framework-${SDK_VERSION}.zip"
+  build_dynamic_fat_framework "${SCHEMA_NAME__ADJUST_WEB_BRIDGE}" "ios" "${XCF_FRM_NAME__ADJUST_WEB_BRIDGE}" "${XCF_OUTPUT_ROOT_FOLDER}" "${TEMP_OUTPUT_FOLDER}" "${ARCHIVE_FILE_NAME}"
 else
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} Skipping iOS (WebBridge) SDK Dynamic Framework build ... ${NC}"
 fi
@@ -67,11 +71,12 @@ fi
 if [[ $BUILD_TARGET_ODM_FRAMEWORK -eq 1 ]]
 then
   TEMP_OUTPUT_FOLDER="${XCF_OUTPUT_ROOT_FOLDER}/${XCF_OUTPUT_DYNAMIC_FRMKS_FOLDER}/${XCF_OUTPUT_FRMK_ODM_FOLDER_DYNAMIC}-framework"
+  ARCHIVE_FILE_NAME="${XCF_OUTPUT_FRMK_ODM_FOLDER_DYNAMIC}-framework-${SDK_VERSION}.zip"
   mkdir -p "${TEMP_OUTPUT_FOLDER}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =${NC}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} XCFramework: Building Dynamic Frameworks for ODM Plugin...${NC}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =${NC}"
-  build_dynamic_fat_framework "${SCHEMA_NAME__ODM_DYNAMIC}" "ios" "${XCF_FRM_NAME__ADJUST_ODM}" "${XCF_OUTPUT_ROOT_FOLDER}" "${TEMP_OUTPUT_FOLDER}" "${XCF_OUTPUT_FRMK_ODM_FOLDER_DYNAMIC}-framework-${SDK_VERSION}.zip"
+  build_dynamic_fat_framework "${SCHEMA_NAME__ODM_DYNAMIC}" "ios" "${XCF_FRM_NAME__ADJUST_ODM}" "${XCF_OUTPUT_ROOT_FOLDER}" "${TEMP_OUTPUT_FOLDER}" "${ARCHIVE_FILE_NAME}"
 else
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} Skipping ODM Plugin Dynamic Framework build ... ${NC}"
 fi
--- scripts/static_frameworks.sh
+++ scripts/static_frameworks.sh
@@ -7,11 +7,12 @@ echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} Static Frameworks build - START... ${NC
 if [[ $BUILD_TARGET_IOS -eq 1 ]]
 then
   TEMP_OUTPUT_FOLDER="${XCF_OUTPUT_ROOT_FOLDER}/${XCF_OUTPUT_STATIC_FRMKS_FOLDER}/${XCF_OUTPUT_FRMK_IOS_FOLDER_STATIC}-framework"
+  ARCHIVE_FILE_NAME="${XCF_OUTPUT_FRMK_IOS_FOLDER_STATIC}-framework-${SDK_VERSION}.zip"
   mkdir -p "${TEMP_OUTPUT_FOLDER}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =${NC}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} XCFramework: Building Static Frameworks for iOS...${NC}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =${NC}"
-  build_static_fat_framework "${SCHEMA_NAME__ADJUST_IOS_STATIC}" "ios" "${XCF_FRM_NAME__ADJUST_IOS}" "${XCF_OUTPUT_ROOT_FOLDER}" "${TEMP_OUTPUT_FOLDER}" "${XCF_OUTPUT_FRMK_IOS_FOLDER_STATIC}-framework-${SDK_VERSION}.zip"
+  build_static_fat_framework "${SCHEMA_NAME__ADJUST_IOS_STATIC}" "ios" "${XCF_FRM_NAME__ADJUST_IOS}" "${XCF_OUTPUT_ROOT_FOLDER}" "${TEMP_OUTPUT_FOLDER}" "${ARCHIVE_FILE_NAME}"
 else
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} Skipping iOS SDK framework build ... ${NC}"
 fi
@@ -19,11 +20,12 @@ fi
 if [[ $BUILD_TARGET_TVOS -eq 1 ]]
 then
   TEMP_OUTPUT_FOLDER="${XCF_OUTPUT_ROOT_FOLDER}/${XCF_OUTPUT_STATIC_FRMKS_FOLDER}/${XCF_OUTPUT_FRMK_TV_FOLDER_STATIC}-framework"
+  ARCHIVE_FILE_NAME="${XCF_OUTPUT_FRMK_TV_FOLDER_STATIC}-framework-${SDK_VERSION}.zip"
   mkdir -p "${TEMP_OUTPUT_FOLDER}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =${NC}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} XCFramework: Building static Frameworks for tvOS...${NC}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =${NC}"
-  build_static_fat_framework "${SCHEMA_NAME__ADJUST_TV_STATIC}" "tvos" "${XCF_FRM_NAME__ADJUST_TV}" "${XCF_OUTPUT_ROOT_FOLDER}" "${TEMP_OUTPUT_FOLDER}" "${XCF_OUTPUT_FRMK_TV_FOLDER_STATIC}-framework-${SDK_VERSION}.zip"
+  build_static_fat_framework "${SCHEMA_NAME__ADJUST_TV_STATIC}" "tvos" "${XCF_FRM_NAME__ADJUST_TV}" "${XCF_OUTPUT_ROOT_FOLDER}" "${TEMP_OUTPUT_FOLDER}" "${ARCHIVE_FILE_NAME}"
 else
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} Skipping tvOS SDK framework build ... ${NC}"
 fi
@@ -31,11 +33,12 @@ fi
 if [[ $BUILD_TARGET_IM -eq 1 ]]
 then
   TEMP_OUTPUT_FOLDER="${XCF_OUTPUT_ROOT_FOLDER}/${XCF_OUTPUT_STATIC_FRMKS_FOLDER}/${XCF_OUTPUT_FRMK_IM_FOLDER_STATIC}-framework"
+  ARCHIVE_FILE_NAME="${XCF_OUTPUT_FRMK_IM_FOLDER_STATIC}-framework-${SDK_VERSION}.zip"
   mkdir -p "${TEMP_OUTPUT_FOLDER}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =${NC}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} XCFramework: Building static Frameworks for iOS (iMessage)...${NC}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =${NC}"
-  build_static_fat_framework "${SCHEMA_NAME__ADJUST_IM_STATIC}" "ios" "${XCF_FRM_NAME__ADJUST_IM}" "${XCF_OUTPUT_ROOT_FOLDER}" "${TEMP_OUTPUT_FOLDER}" "${XCF_OUTPUT_FRMK_IM_FOLDER_STATIC}-framework-${SDK_VERSION}.zip"
+  build_static_fat_framework "${SCHEMA_NAME__ADJUST_IM_STATIC}" "ios" "${XCF_FRM_NAME__ADJUST_IM}" "${XCF_OUTPUT_ROOT_FOLDER}" "${TEMP_OUTPUT_FOLDER}" "${ARCHIVE_FILE_NAME}"
 else
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} Skipping iMessage SDK framework build ... ${NC}"
 fi
@@ -43,11 +46,12 @@ fi
 if [[ $BUILD_TARGET_WEB_BRIDGE -eq 1 ]]
 then
   TEMP_OUTPUT_FOLDER="${XCF_OUTPUT_ROOT_FOLDER}/${XCF_OUTPUT_STATIC_FRMKS_FOLDER}/${XCF_OUTPUT_FRMK_WEB_BRIDGE_FOLDER_STATIC}-framework"
+  ARCHIVE_FILE_NAME="${XCF_OUTPUT_FRMK_WEB_BRIDGE_FOLDER_STATIC}-framework-${SDK_VERSION}.zip"
   mkdir -p "${TEMP_OUTPUT_FOLDER}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =${NC}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} XCFramework: Building static Frameworks for iOS (WebBridge)...${NC}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =${NC}"
-  build_static_fat_framework "${SCHEMA_NAME__ADJUST_WEB_BRIDGE_STATIC}" "ios" "${XCF_FRM_NAME__ADJUST_WEB_BRIDGE}" "${XCF_OUTPUT_ROOT_FOLDER}" "${TEMP_OUTPUT_FOLDER}" "${XCF_OUTPUT_FRMK_WEB_BRIDGE_FOLDER_STATIC}-framework-${SDK_VERSION}.zip"
+  build_static_fat_framework "${SCHEMA_NAME__ADJUST_WEB_BRIDGE_STATIC}" "ios" "${XCF_FRM_NAME__ADJUST_WEB_BRIDGE}" "${XCF_OUTPUT_ROOT_FOLDER}" "${TEMP_OUTPUT_FOLDER}" "${ARCHIVE_FILE_NAME}"
 else
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} Skipping iOS (WebBridge) SDK framework build ... ${NC}"
 fi
@@ -55,11 +59,12 @@ fi
 if [[ $BUILD_TARGET_ODM_FRAMEWORK -eq 1 ]]
 then
   TEMP_OUTPUT_FOLDER="${XCF_OUTPUT_ROOT_FOLDER}/${XCF_OUTPUT_STATIC_FRMKS_FOLDER}/${XCF_OUTPUT_FRMK_ODM_FOLDER_STATIC}-framework"
+  ARCHIVE_FILE_NAME="${XCF_OUTPUT_FRMK_ODM_FOLDER_STATIC}-framework-${SDK_VERSION}.zip"
   mkdir -p "${TEMP_OUTPUT_FOLDER}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =${NC}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} XCFramework: Building static Frameworks for ODM Plugin...${NC}"
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =${NC}"
-  build_static_fat_framework "${SCHEMA_NAME__ODM_STATIC}" "ios" "${XCF_FRM_NAME__ADJUST_ODM}" "${XCF_OUTPUT_ROOT_FOLDER}" "${TEMP_OUTPUT_FOLDER}" "${XCF_OUTPUT_FRMK_ODM_FOLDER_STATIC}-framework-${SDK_VERSION}.zip"
+  build_static_fat_framework "${SCHEMA_NAME__ODM_STATIC}" "ios" "${XCF_FRM_NAME__ADJUST_ODM}" "${XCF_OUTPUT_ROOT_FOLDER}" "${TEMP_OUTPUT_FOLDER}" "${ARCHIVE_FILE_NAME}"
 else
   echo -e "${CYAN}[ADJUST][BUILD]:${GREEN} Skipping ODM plugin framework build ... ${NC}"
 fi
